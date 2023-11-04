db793784e4465e2d4af9b88ce43368eeae597373
ConnorCoderAcademy <13716@coderacademy.edu.au>
2023-11-04 12:10:21 +1100

    render Trello data and margin fix in CSS

--- script.js
+++ script.js
@@ -12,6 +12,26 @@ let trelloData = {
                     timestamp: null
                 }
             ]
+        },
+        {
+            name: "Doing",
+            cards: [
+                {
+                    title: "Lesson in progress",
+                    content: "Learning JS events",
+                    timestamp: null
+                }
+            ]
+        },
+        {
+            name: "Done",
+            cards: [
+                {
+                    title: "HTML & CSS",
+                    content: "Done done",
+                    timestamp: null
+                }
+            ]
         }
     ]
 }
@@ -22,6 +42,41 @@ function renderColumns() {
     let trelloDataRowRootNode = document.getElementById("dataDisplayRow");
     //Removing any stale or old HTML content
     trelloDataRowRootNode.innerHTML = "";
+    //generate new HTML content 
+    trelloData.columns.forEach((column) => {
+        console.log(column.name)
+
+        //create the element to contain the column
+        let columnNode = document.createElement("div");
+        columnNode.classList.add("trelloColumn");
+
+        //create content to render column data
+        let columnHeading = document.createElement("h3");
+        columnHeading.innerText = column.name;
+        columnNode.appendChild(columnHeading);
+
+        //create the cards 
+        column.cards.forEach((card) => {
+            //find the card preview, copy it, and save the copy to the variable
+            let newCard = document.getElementById("cardPreview").cloneNode(true);
+
+            if (!card.timestamp || isNaN(card.timestamp)) {
+                card.timestamp = Date.now();
+            }
+
+            newCard.id = card.timestamp;
+            
+            //find the h3 of the card title and change its content 
+            newCard.querySelector(".cardDisplay-title").innerText = card.title;
+            //Same as the above for the paragraph
+            newCard.querySelector(".cardDisplay-content").innerText = card.content;
+            //after data is all done, attach card to the column
+            columnNode.appendChild(newCard)
+        })
+
+        //after column is created, append it to each node as a child
+        trelloDataRowRootNode.appendChild(columnNode)
+    })
 }
 
 renderColumns();
